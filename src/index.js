9207c5aa05b1c6db0cb7aa0bc63ffed6c11d8122
Sean Huang <hyprul@ucla.edu>
2019-11-28 19:31:45 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <div>
-    <h1>My Favourite Foods</h1>
+function Heading() {
+  return <h1>My Favourite Foods</h1>;
+}
+
+function List() {
+  return (
     <ul>
       <li>Bacon</li>
       <li>Jamon</li>
       <li>Noodles</li>
     </ul>
+  );
+}
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <List />
   </div>,
   document.getElementById("root")
 );
